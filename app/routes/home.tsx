5db0d2f8ba101ddef3367683cd949c30771c3ed8
Chris Brannen <cbrannen@outlook.com>
2025-09-24 16:16:44 +0100

    feat: initial setup

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,17 +1,17 @@
-import type { Route } from "./+types/home";
 import { Welcome } from "../welcome/welcome";
+import type { Route } from "./+types/home";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Forged Hand Studios" },
+    { name: "description", content: "Welcome to Forged Hand Studios!" },
   ];
 }
 
 export function loader({ context }: Route.LoaderArgs) {
-  return { message: context.VALUE_FROM_NETLIFY };
+  return {};
 }
 
 export default function Home({ loaderData }: Route.ComponentProps) {
-  return <Welcome message={loaderData.message} />;
+  return <Welcome />;
 }
